9e634042b8102c8ce73df0f9df34825e0785c992
mcatal-d <catalmathis@gmail.com>
2023-11-04 14:24:05 +0100

    sans tous les commentaires du www

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+# Démarre le service MariaDB.
 service mariadb start;
 
+# Pause d'une seconde pour assurer que MariaDB a démarré.
 sleep 1
 
+# Crée une base de données MySQL si elle n'existe pas déjà.
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# Crée un utilisateur MySQL s'il n'existe pas déjà.
 mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# Accorde tous les privilèges à l'utilisateur sur la base de données.
 mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# Modifie le mot de passe de l'utilisateur 'root' de MySQL.
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+# Actualise les privilèges pour prendre en compte les modifications.
 mariadb -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
+# Arrête proprement le service MariaDB.
 mysqladmin -u root --password=${SQL_ROOT_PASSWORD} shutdown
 
+# Démarre MariaDB en mode sécurisé (safe mode).
 mysqld_safe
 
-echo "mariaDB were create successfully!!"
+# Affiche un message pour indiquer que MariaDB a été créé avec succès.
+echo "MariaDB a été créé avec succès!!"
